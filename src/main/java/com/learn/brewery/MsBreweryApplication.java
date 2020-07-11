2290dbdad3208425f6290a76e7412a1ba6bf400b
Ruchir Agrawal <masterruchir.18@gmail.com>
2020-07-12 00:43:06 +0530

    Base Code Created

---
+++ src/main/java/com/learn/brewery/MsBreweryApplication.java
@@ -0,0 +1,13 @@
+package com.learn.brewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBreweryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/brewery/MsBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.brewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
